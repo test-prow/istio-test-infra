19be3609ead0a1231b27b6794c0b1a4b29105045
dex <dex@aspenmesh.io>
2020-05-06 17:24:45 -0600

    Add env vars for DOCKER_HUB for release jobs (#7)
    * Add job configs for cni and proxy repos
    
    * Add DOCKER_HUB evn vars for uploading docker images

--- prow/recreate_istio_jobconfig.sh
+++ prow/recreate_istio_jobconfig.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-# (re) create job-config of Prow using yaml files under test-infra/prow/cluster/jobs/
+# (re) create job-config of Prow using yaml files under test-infra/prow/cluster/jobs/aspenmesh
 #
-kubectl create configmap job-config --from-file=all-presets.yaml=cluster/jobs/all-presets.yaml \
- --from-file=istio.istio.release-1.6.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.6.gen.yaml \
- --from-file=istio.istio.master.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.master.gen.yaml \
- --from-file=istio.istio.release-1.4.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.4.gen.yaml \
- --from-file=istio.istio.release-1.5.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.5.gen.yaml \
- --dry-run=client -o yaml | kubectl replace configmap job-config -f -
+ kubectl create configmap job-config --from-file=all-presets.yaml=cluster/jobs/all-presets.yaml \
+ --from-file=istio.proxy.release-1.4.yaml=cluster/jobs/aspenmesh/proxy/istio.proxy.release-1.4.yaml \
+ --from-file=istio.proxy.release-1.5.gen.yaml=cluster/jobs/aspenmesh/proxy/istio.proxy.release-1.5.gen.yaml   \
+ --from-file=istio.proxy.master.gen.yaml=cluster/jobs/aspenmesh/proxy/istio.proxy.master.gen.yaml   \
+ --from-file=istio.proxy.release-1.6.gen.yaml=cluster/jobs/aspenmesh/proxy/istio.proxy.release-1.6.gen.yaml   \
+ --from-file=istio.istio.release-1.6.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.6.gen.yaml   \
+ --from-file=istio.istio.master.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.master.gen.yaml   \
+ --from-file=istio.istio.release-1.4.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.4.gen.yaml   \
+ --from-file=istio.istio.release-1.5.gen.yaml=cluster/jobs/aspenmesh/istio/istio.istio.release-1.5.gen.yaml   \
+ --from-file=istio.cni.release-1.6.gen.yaml=cluster/jobs/aspenmesh/cni/istio.cni.release-1.6.gen.yaml   \
+ --from-file=istio.cni.release-1.4.gen.yaml=cluster/jobs/aspenmesh/cni/istio.cni.release-1.4.gen.yaml   \
+ --from-file=istio.cni.release-1.5.gen.yaml=cluster/jobs/aspenmesh/cni/istio.cni.release-1.5.gen.yaml   \
+ --from-file=istio.cni.master.gen.yaml=cluster/jobs/aspenmesh/cni/istio.cni.master.gen.yaml   --dry-run=client -o yaml | kubectl replace configmap job-config -f -
